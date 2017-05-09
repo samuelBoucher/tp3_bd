484de93eda41a267ae881e7e765c4100630067e9
samuelBoucher <samuel.boucher@outlook.com>
2017-05-09 15:18:15 -0400

    context

---
+++ src/TP3/DataAccessLayer/HedgesProductionsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TP3.Entities;
+
+namespace TP3.DataAccessLayer
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Artiste> Artistes { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Contrat> Contrats { get; set; }
+        public DbSet<Facture> Factures { get; set; }
+        public DbSet<Groupe> Groupes { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+    }
+}
