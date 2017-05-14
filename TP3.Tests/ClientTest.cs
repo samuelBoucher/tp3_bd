05d5d9cbb3ce3b758be03fb1e55753d9cdb4960f
samuelBoucher <samuel.boucher@outlook.com>
2017-05-14 18:17:56 -0400

    Add_ShouldAddClientToDatabase

---
+++ TP3.Tests/ClientTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using FluentAssertions;
+using TP3.DataAccessLayer;
+using TP3.Entities;
+using Xunit;
+
+namespace TP3.Tests
+{
+    public class ClientTest
+    {
+        private readonly HedgesContextFactory _contextFactory;
+        private readonly ClientEntityFramework _repository;
+
+        public ClientTest()
+        {
+            _contextFactory = new HedgesContextFactory();
+
+            var dbContext = _contextFactory.Create();
+            ClearAllTables(dbContext);
+            _repository = new ClientEntityFramework(dbContext);
+        }
+
+        private static void ClearAllTables(HedgesProductionsContext dbContext)
+        {
+            dbContext.Clients.RemoveRange(dbContext.Clients);
+            dbContext.SaveChanges();
+        }
+
+
+        [Fact]
+        public void Add_ShouldAddClientToDatabase()
+        {
+            var client = new Client()
+            {
+                CodeClient = "Code",
+                Prenom = "Prenom",
+                Nom = "Nom",
+                NoTelephone = 123456789,
+                DepotNecessaire = false
+            };
+            _repository.AddClient(client);
+
+            using (var apiDbContext = _contextFactory.Create())
+            {
+                apiDbContext.Clients.ToList().Count.Should().Be(1);
+            }
+        }
+    }
+}
---
+++ src/TP3/DataAccessLayer/ClientEntityFramework.cs
@@ -0,0 +1,22 @@
+﻿using TP3.Entities;
+
+namespace TP3.DataAccessLayer
+{
+    public class ClientEntityFramework
+    {
+        private readonly HedgesProductionsContext _context;
+
+        public ClientEntityFramework(HedgesProductionsContext context)
+        {
+            _context = context;
+        }
+
+        public void AddClient(Client client)
+        {
+            _context.Clients.Add(client);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
